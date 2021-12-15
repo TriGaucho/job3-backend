65dca7bd2f32fe21f5e118740da7608cc6adca6b
diogo.almeida <diogo.almeida@compasso.com.br>
2021-12-15 13:37:39 -0300

    Liberação de CORS

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
-import App from './app'
 import * as express from 'express'
+import * as cors from 'cors'
+
+import App from './app'
 import * as Router from './routers'
 
+
 const server = express()
-server.use('/', Router)
 const port = process.env.PORT || 8081
+
+server.use(cors({ 
+    origin: "*",
+    credentials: true,
+    methods: "GET, OPTIONS, POST, PUT"
+  })
+)
+
+server.use('/', Router)
 
 server.listen(port, () => {
   console.info(`Aplicação carregada na porta ${port}`)
