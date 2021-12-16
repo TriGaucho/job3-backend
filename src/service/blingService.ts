252541bc6cac30f001fa9cee96c69c6270a51335
diogo.almeida <diogo.almeida@compasso.com.br>
2021-12-16 10:48:12 -0300

    Configurado variavel de ambiente

--- src/service/blingService.ts
+++ src/service/blingService.ts
@@ -1,10 +1,14 @@
 import axios from 'axios'
+require('dotenv').config()
 
 export default class BlingService {
     static async getBling(): Promise<any> {
-        const response = await axios.get('https://bling.com.br/Api/v2/produtos/json/?apikey=19990fa5b69f53dbe8b9f4e23ccf4afd4ed408b4dccc54e9bbebfd9c957c60f40ffcabc2')
+      try{
+        const response = await axios.get(`https://bling.com.br/Api/v2/produtos/json/?apikey=${process.env.APIKEY}`)
         console.log(response.data)
-
         return response.data
+        }catch{
+           throw {msd: 'erro'} 
+        }
     }
 }
